61850fd0fb925d9a1085d1a28ec0efc81414e8ee
diek-lab <marcelzintl@googlemail.com>
2020-03-11 19:57:02 +0100

    2. Test

--- src/main/java/de/ratzfratz/ratzfratz/App.java
+++ src/main/java/de/ratzfratz/ratzfratz/App.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
 	public static void main(final String[] args) {
-		System.out.println("Hello");
 		SpringApplication.run(App.class, args);
 	}
 
